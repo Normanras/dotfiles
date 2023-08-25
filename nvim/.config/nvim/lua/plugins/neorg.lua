89abb2744fd5804787445146e317711b3c27bf04
Norm Rasmussen <norman.rasmussen@me.com>
2023-08-25 16:42:08 -0400

    More Neorg Config. Working on getting this templates WIP plugin set up, but not working yet.

--- nvim/.config/nvim/lua/plugins/neorg.lua
+++ nvim/.config/nvim/lua/plugins/neorg.lua
@@ -1,15 +1,29 @@
  return {
     "nvim-neorg/neorg",
     build = ":Neorg sync-parsers",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      { "nvim-lua/plenary.nvim",
+        "pysan3/neorg-templates", dependencies = { "L3MON4D3/LuaSnip" },
+    },
+  },
     config = function()
       require("neorg").setup {
         load = {
           ["core.defaults"] = {},
           ["core.concealer"] = {},
 	  ["core.integrations.nvim-cmp"] = {},
+	  ["core.integrations.treesitter"] = {},
+	  ["external.templates"] = {},
 	  ["core.highlights"] = {},
 	  ["core.mode"] = {},
+	  ["core.itero"] = {},
+	  ["core.journal"] = {
+	    config = {
+	      template_name = "template.norg",
+	      use_template = "true",
+	      workspace = "customer_notes",
+	    }
+	  },
 	  ["core.keybinds"] = {
 	    config = {
 	      hook =  function(keybinds)
