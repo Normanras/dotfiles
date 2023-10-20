3c344bc893d7348d649f5b2f3f2c39791238a0a0
Norm Rasmussen <norman.rasmussen@me.com>
2023-10-20 16:39:07 -0400

    Figured out Pulse. Want to add to that by using nvim-notify.

---
+++ nvim/.config/nvim/lua/plugins/pulse.lua
@@ -0,0 +1,16 @@
+return {
+  'linguini1/pulse.nvim',
+    version = "*",
+	config = function()
+
+	  local pulse = require("pulse")
+
+	  pulse.setup({level = vim.log.levels.DEBUG})
+
+		  pulse.add("Send Krista Message", {
+			interval = 60,
+			message = "Tell Krista that you Love Her!",
+			enabled = True,
+	  })
+  end
+}
