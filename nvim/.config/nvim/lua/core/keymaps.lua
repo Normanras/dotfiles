f20db3b89f38b926d5f1bfeeeea9d7c9dea9f093
Norm Rasmussen <norman.rasmussen@me.com>
2025-01-03 16:44:22 -0500

    Fixed secrets issues

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -2,8 +2,7 @@
 -- Keymaps of Neovim and installed plugins
 -----------------------------------------------------------
 
--- local map = vim.api.nvim_set_keymap
-local map = vim.keymap.set
+local map = vim.api.nvim_set_keymap
 local default_opts = { noremap = true, silent = true }
 
 -- Fast saving with <leader> and s
@@ -17,14 +16,13 @@ local default_opts = { noremap = true, silent = true }
 -- Close and save all buffers and return to Dashboard
 map('n', '<leader>ds', ':silent wa | %bd | Alpha', default_opts)
 
--- Save and close current buffer
-map('n', '<leader>bd', ':w | bp | sp | bn | bd | Alpha', default_opts)
-
 -- Neovim Tab Navgation via Vem-Tabline
 map('n', '<leader>t', ':tabnew<CR>', default_opts)
 map('n', '<leader>e', ':bnext<CR>', default_opts)
 map('n', '<leader>w', ':bprev<CR>', default_opts)
 map('n', '<leader>d', ':bdelete<CR>', default_opts)
+map('n', '<leader>r', '<Plug>vem_move_buffer_right', default_opts)
+map('n', '<leader>q', '<Plug>vem_move_buffer_left', default_opts)
 
 -- Nvim-Tree
 map('n', '<leader>v', ':NvimTreeToggle<CR>', default_opts)
@@ -43,8 +41,6 @@ map('n', '<leader>cb', ':Telescope current_buffer_fuzzy_find<CR>', default_opts)
 map('n', '<leader>b', ':! black %<CR>', default_opts)
 map('n', '<leader>m', ':! markdownlint -f %<CR>', default_opts)
 map('n', '<leader>pj', ':!python -m json.tool<CR>', default_opts)
-
--- Forget what this one does below, but I think it's for CSV reformatting.
 map('n', '<leader>cs', ':%s/\\s*\\([^|]\\{-}\\)\\s*|/"\1",/g', default_opts)
 
 -- Fold all comments
@@ -54,5 +50,41 @@ map('n', '<leader>fc', ':set foldmethod=expr foldexpr=getline(v:lnum)=~"^\\s*".&
 map('n', '<C-t>', ':ToggleTerm direction=float<CR>', default_opts)
 map('t', '<C-n>', '<C-\\><C-n><CR>', default_opts) -- Exit Insert Mode Faster
 
--- Cycle through the number line options
-map('n', '<leader>n', ":set relativenumber!<CR>", default_opts)
+------------------------------------------------
+-- Old Keymaps from Nvim-Mapper (Sunsetted)
+------------------------------------------------
+--[[
+-- Macros for Todo Trouble
+M('n', '<C-t>', "@t<CR>", default_opts,
+"Add Todo", "todo_todo", "Add To-do/Task to the beginning of the line"
+)
+
+M('n', '<C-s>', "@s<CR>", default_opts,
+"Add Solutions Engineering", "todo_seng", "Add Solutions Engineering to the beginning of the line"
+)
+
+M('n', '<C-f>', "@f<CR>", default_opts,
+"Add Feature", "add_feat", "Add Feature Request tag to the beginning of the line. "
+)
+
+M( 'n', '<C-x>', "@c<CR>", default_opts,
+"Replace with Complete", "add_complete", "Replace tag with Complete tag at beginning of the line."
+)
+
+M('n', '<C-r>', "@w<CR>", default_opts,
+"Add Warning/Error", "add_error", "Add Warning/Error tag at the beginning of the line."
+)
+
+M('n', '<leader>ce', ":TodoTrouble keywords=TODO<CR>", default_opts,
+"Show Todo Tags", "show_todos", "Show Todo Tags."
+)
+
+M('n', '<leader>cf', ":TodoTrouble keywords=FEAT<CR>", default_opts,
+"Show Feature Tags", "show_features", "Show Feature Requests."
+)
+
+M('n', '<leader>cq', ":TodoTrouble keywords=ERROR, WARN<CR>", default_opts,
+"Show Warning Tags", "show_warnings", "Show Errors Tags."
+)
+
+--]]
--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,58 +1,49 @@
 return {
   'saghen/blink.cmp',
   lazy = false, -- lazy loading handled internally
-	version = '*',
+  -- optional: provides snippets for the snippet source
   dependencies = {
 									'rafamadriz/friendly-snippets',
-									'L3MON4D3/LuaSnip', version = 'v2.*',
+									'L3MON4D3/LuaSnip',
 									},
+  version = 'v0.*',
   opts = {
 		keymap = {
-      preset = "default",
-				['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-				['<C-e>'] = { 'hide', 'fallback' },
-				['<Tab>'] = {
-					function(cmp)
-						if cmp.snippet_active() then return cmp.accept()
-						else return cmp.select_and_accept() end
-					end,
-					'snippet_forward',
-					'fallback'
-				},
-				['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+      preset = "enter",
 
-				['<Up>'] = { 'select_prev', 'fallback' },
-				['<Down>'] = { 'select_next', 'fallback' },
-				['<C-p>'] = { 'select_prev', 'fallback' },
-				['<C-n>'] = { 'select_next', 'fallback' },
+      ["<Tab>"] = {
+        "select_next",
+        "snippet_forward",
+        "fallback",
+      },
 
-				['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-				['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+      ["<S-Tab>"] = {
+        "select_prev",
+        "snippet_backward",
+        "fallback",
+      },
     },
-		appearance = {
-			use_nvim_cmp_as_default = true,
-			nerd_font_variant = 'normal',
-		},
-		completion = {
-			menu = { auto_show = function(ctx) return ctx.mode ~= 'cmdline' end },
-			-- keyword = { range = 'prefix', regex = '[%w_\\-]'},
-			-- trigger = { show_on_blocked_trigger_characters = function()
-			-- 							if vim.api.nvim_get_mode().mode == 'c' then return {} end
-			-- 								return { ' ', '\n', '\t' }
-			-- 							end },
-		},
-		snippets = {
-      expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
-      active = function(filter)
-        if filter and filter.direction then
-          return require('luasnip').jumpable(filter.direction)
-        end
-        return require('luasnip').in_snippet()
-      end,
-      jump = function(direction) require('luasnip').jump(direction) end,
-    },
+
+    highlight = {
+      use_nvim_cmp_as_default = true,
+					nerd_font_variant = 'normal',
+				}
+			},
+		trigger = {
+			completion = {
+							keyword_regex = '[%w_\\-]',
+							blocked_trigger_characters = { ' ', '\n', '\t' },
+							show_on_insert_on_trigger_character = true,
+        }
+						},
 		 sources = {
-			default = {'lsp', 'path', 'snippets', 'buffer'},
-			     },
+						providers = {
+							{
+								{ 'blink.cmp.sources.lsp' },
+								{ 'blink.cmp.sources.path' },
+								{ 'blink.cmp.sources.snippets', score_offset = -1 },
+							},
+							{ { 'blink.cmp.sources.buffer' } },
+					}
+        },
 	}
-}
